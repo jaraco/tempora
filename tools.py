49231fe9d0fedb658d33c0b84644833b1ebe4af4
jaraco <none@none>
2005-01-25 16:29:27 +0000

    Created a DateTime parser (DTParser) for South Asia.  This will allow users to enter less regimented dates/times.

--- tools.py
+++ tools.py
@@ -469,3 +469,47 @@ class ciDict( dict ):
 		if isinstance( key, basestring ):
 			key = ciString( key )
 		dict.__setitem__( self, key, val )
+
+class DTParser( object ):
+	"""Datetime parser: parses a date-time string using multiple possible formats.
+	>>> p = DTParser( ( '%H%M', '%H:%M' ) )
+	>>> p.parse( '1319' )
+	(1900, 1, 1, 13, 19, 0, 0, 1, -1)
+	>>> dateParser = DTParser( ( '%m/%d/%Y', '%Y-%m-%d', '%d-%b-%Y' ) )
+	>>> dateParser.parse( '2003-12-20' )
+	(2003, 12, 20, 0, 0, 0, 5, 354, -1)
+	>>> dateParser.parse( '16-Dec-1994' )
+	(1994, 12, 16, 0, 0, 0, 4, 350, -1)
+	>>> dateParser.parse( '5/19/2003' )
+	(2003, 5, 19, 0, 0, 0, 0, 139, -1)
+	>>> dtParser = tools.DTParser( ( '%Y-%m-%d %H:%M:%S', '%a %b %d %H:%M:%S %Y' ) )
+	>>> dtParser.parse( '2003-12-20 19:13:26' )
+	(2003, 12, 20, 19, 13, 26, 5, 354, -1)
+	>>> dtParser.parse( 'Tue Jan 20 16:19:33 2004' )
+	(2004, 1, 20, 16, 19, 33, 1, 20, -1)
+	Be forewarned:
+	>>> DTParser( ( '%H%M', '%H%M%S' ) ).parse( '732' )
+	ValueError: More than one format string matched target 732.
+	"""
+	formats = ( )
+	def __init__( self, formats = None ):
+		if formats:
+			self.formats = formats
+
+	def parse( self, target ):
+		self.target = target
+		results = filter( None, map( self._parse, self.formats ) )
+		del self.target
+		if not results:
+			raise ValueError, "No format strings matched the target %s." % target
+		if not len( results ) == 1:
+			raise ValueError, "More than one format string matched target %s." % target
+		return results[0]
+
+	def _parse( self, format ):
+		try:
+			result = time.strptime( self.target, format )
+		except ValueError:
+			result = False
+		return result
+	
