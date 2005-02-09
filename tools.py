d52bf14727e4ca5006005193b60b917f94fd6241
SANDIA\jaraco <none@none>
2005-02-09 23:26:13 +0000

    Fixed all of the module tags to use svnauthor instead of vssauthor. Changed all to use svn keywords.

--- tools.py
+++ tools.py
@@ -6,9 +6,9 @@ tools.py:
 """
 
 __author__ = 'Jason R. Coombs <jaraco@sandia.gov>'
-__version__ = '$Revision: 36 $'[11:-2]
-__vssauthor__ = '$Author: Jaraco $'[9:-2]
-__date__ = '$Modtime: 27-09-04 11:24 $'[10:-2]
+__version__ = '$Rev: 36 $'[6:-2]
+__svnauthor__ = '$Author: Jaraco $'[9:-2]
+__date__ = '$Date: 27-09-04 11:24 $'[7:-2]
 
 import string, urllib, os
 import logging
