fb208c1d8302c086a0e655665fc2ad13fd78afa3
Jason R. Coombs <jaraco@jaraco.com>
2015-04-25 13:10:57 -0400

    Add date_range

--- tempora.py
+++ tempora.py
@@ -421,3 +421,25 @@ def divide_timedelta(td1, td2):
 		# Python 3.2 gets division
 		# http://bugs.python.org/issue2706
 		return td1.total_seconds() / td2.total_seconds()
+
+def date_range(start=None, stop=None, step=None):
+	"""
+	Much like the built-in function range, but works with dates
+
+	>>> range_items = date_range(
+	...     datetime.datetime(2005,12,21),
+	...     datetime.datetime(2005,12,25),
+	... )
+	>>> my_range = tuple(range_items)
+	>>> datetime.datetime(2005,12,21) in my_range
+	True
+	>>> datetime.datetime(2005,12,22) in my_range
+	True
+	>>> datetime.datetime(2005,12,25) in my_range
+	False
+	"""
+	if step is None: step = datetime.timedelta(days=1)
+	if start is None: start = datetime.datetime.now()
+	while start < stop:
+		yield start
+		start += step
