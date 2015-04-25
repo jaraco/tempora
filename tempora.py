2188a23bff865b6850e565dfed5df2277f5392cb
Jason R. Coombs <jaraco@jaraco.com>
2015-04-25 12:36:08 -0400

    Update docstring as found in svg.charts

--- tempora.py
+++ tempora.py
@@ -351,8 +351,13 @@ def get_date_format_string(period):
 def divide_timedelta_float(td, divisor):
 	"""
 	Divide a timedelta by a float value
-	>>> divide_timedelta_float(datetime.timedelta(days=1), 2)
-	datetime.timedelta(0, 43200)
+
+	>>> one_day = datetime.timedelta(days=1)
+	>>> half_day = datetime.timedelta(days=.5)
+	>>> divide_timedelta_float(one_day, 2.0) == half_day
+	True
+	>>> divide_timedelta_float(one_day, 2) == half_day
+	True
 	"""
 	# td is comprised of days, seconds, microseconds
 	dsm = [getattr(td, attr) for attr in ('days', 'seconds', 'microseconds')]
