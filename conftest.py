ba67f2bc1a484452d1e4caf537e71ef54c88b1a2
Jason R. Coombs <jaraco@jaraco.com>
2018-03-13 11:34:08 -0400

    Skip doctest failing on older Pythons.

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import sys
+import operator
+
+
+def pytest_collection_modifyitems(session, config, items):
+	remove_parse_timedelta(items)
+
+
+def remove_parse_timedelta(items):
+	"""
+	Repr on older Pythons is different, so remove the offending
+	test.
+	"""
+	if sys.version_info > (3, 7):
+		return
+	names = list(map(operator.attrgetter('name'), items))
+	del items[names.index('tempora.parse_timedelta')]
