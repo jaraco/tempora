591d03f1988a4c0c06e05165fca443a1863a7c4d
Jason R. Coombs <jaraco@jaraco.com>
2019-12-20 00:12:12 -0500

    Add test for calculate_prorated_values

--- tempora/__init__.py
+++ tempora/__init__.py
@@ -357,7 +357,18 @@ def divide_timedelta_float(td, divisor):
     return datetime.timedelta(*dsm)
 
 
-def calculate_prorated_values():  # pragma: nocover
+def calculate_prorated_values():
+    """
+    >>> monkeypatch = getfixture('monkeypatch')
+    >>> import builtins
+    >>> monkeypatch.setattr(builtins, 'input', lambda prompt: '3/hour')
+    >>> calculate_prorated_values()
+    per minute: 0.05
+    per hour: 3.0
+    per day: 72.0
+    per month: 2191.454166666667
+    per year: 26297.45
+    """
     rate = input("Enter the rate (3/hour, 50/month)> ")
     for period, value in _prorated_values(rate):
         print("per {period}: {value}".format(**locals()))
