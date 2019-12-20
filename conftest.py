dc72929838cb0f346ce0519c2fe40d06f3445d11
Jason R. Coombs <jaraco@jaraco.com>
2019-12-20 00:12:59 -0500

    Clean up conftest

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ def pytest_collection_modifyitems(session, config, items):
     remove_parse_timedelta(items)
 
 
-def remove_parse_timedelta(items):
+def remove_parse_timedelta(items):  # pragma: nocover
     """
     Repr on older Pythons is different, so remove the offending
     test.
@@ -15,10 +15,3 @@ def remove_parse_timedelta(items):
         return
     names = list(map(operator.attrgetter('name'), items))
     del items[names.index('tempora.parse_timedelta')]
-
-
-collect_ignore = []
-
-
-if sys.version_info < (3, 2):
-    collect_ignore.extend(['tempora/utc.py'])
