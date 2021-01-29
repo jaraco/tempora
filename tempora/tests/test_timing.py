14d532af265e35af33a28d61a68d545993fc5b78
Jason R. Coombs <jaraco@jaraco.com>
2021-01-29 09:51:23 -0500

    Use pytest.mark to selectively skip test.

--- tempora/tests/test_timing.py
+++ tempora/tests/test_timing.py
@@ -24,11 +24,9 @@ def test_IntervalGovernor():
     func_under_test.assert_called_once_with('a')
 
 
+@pytest.mark.skipif("not hasattr(time, 'tzset')")
 @pytest.fixture
 def alt_tz(monkeypatch):
-    if not hasattr(time, 'tzset'):
-        pytest.skip("tzset not available")
-
     @contextlib.contextmanager
     def change():
         val = 'AEST-10AEDT-11,M10.5.0,M3.5.0'
